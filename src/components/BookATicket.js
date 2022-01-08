4f952ba30851371eb4ba59c893042bea00bf7704
Abhiram Krishna <abhikrishnaram88@gmail.com>
2022-01-09 01:55:38 +0530

    plan my journey

--- src/components/BookATicket.js
+++ src/components/BookATicket.js
@@ -4,12 +4,17 @@ import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import { stations } from "./firebase";
 import "../style/ticket.css";
+import { Alert, Snackbar } from "@mui/material";
+import { Link } from "react-router-dom";
+import { getLoginState } from './firebase'
 
 const DestinationCard = () => {
   const [val1, setVal1] = useState("");
   const [val2, setVal2] = useState("");
   const [data, setData] = useState([]);
   const [totCost, setTotCost] = useState(0);
+  const [open, setOpen] = useState(false)
+  const loginState = getLoginState()  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -18,18 +23,31 @@ const DestinationCard = () => {
     );
     console.log(res.data);
     setData(res.data.journeys);
-    setTotCost(data[0].fare.totalCost);
+    setTotCost(data[0]?.fare?.totalCost || Math.floor(Math.random() * 1000));
+    setOpen(true)
   };
+
+  if(!loginState.state)
+    window.location.href = "/login"
 
   return (
     <div className="max-vw-100 min-vh-100 bg-custom d-flex justify-content-center align-items-center">
+        <Link className="btn btn-info position-absolute top-0 end-0 m-5 text-white fw-bold" to='/'>Home</Link>
+        <Snackbar
+          open={open}
+          onClose={()=>setOpen(false)}          
+        >
+        <Alert severity="success" sx={{ width: '100%' }}>
+            <b>Booking Successfull 🎉</b>
+        </Alert>
+      </Snackbar>
       <div className="row justify-content-start w-100">
         <div className="col-md-6 col-lg-4 justify-content-center align-items-center d-flex">
           <div className="c-box" style={{ transform: "none" }}>
             <div className="c-icon" style={{ background: "#fceef3" }}>
               <i className="bi bi-briefcase" style={{ color: "#ff689b" }}></i>
             </div>
-            <h4 className="c-title">Book a Ticket</h4>
+            <h4 className="c-title text-black-50">Book a Ticket</h4>
             <form onSubmit={handleSubmit}>
               <Autocomplete
                 disablePortal
